1cb2779e8021c437972f7a895d8321078874569e
flamhaze5946 <flamhaze5946@gmail.com>
2021-11-10 23:04:13 +0800

    modify pusher clean logic

--- pusher.py
+++ pusher.py
@@ -13,7 +13,6 @@ from config import klines_web_fetch_worker, timezone, save_buffer_millseconds
 from fetcher import CcxtFetcher
 
 max_workers = klines_web_fetch_worker
-last_interval_time = {}
 
 
 class Pusher(object):
@@ -33,7 +32,7 @@ class Pusher(object):
         self._scheduler = BlockingScheduler(timezone=timezone)
         self._save_klines_thread_pool = ThreadPoolExecutor(max_workers=max_workers)
 
-    def _push(self, klines: list):
+    def _push(self, klines: List[list]):
         pass
 
     def _clean(self, interval: str, symbols: List[str], save_days: int):
@@ -42,7 +41,7 @@ class Pusher(object):
     def _query_klines_by_start_time(self, interval: str, symbol: str, start_time: int):
         pass
 
-    def _set_kline(self, interval: str, symbol: str, kline, kline_time: int, remove: bool, insert: bool):
+    def _set_kline(self, interval: str, symbol: str, kline: list, kline_time: int, remove: bool, insert: bool):
         pass
 
     def _save_klines(self, interval: str, symbols: List[str], bar_count: int = 99):
@@ -52,35 +51,18 @@ class Pusher(object):
             futures.append(future)
         [future.result() for future in futures]
 
-    def _save_klines0(self, interval: str, symbol: str, klines: list):
+    def _save_klines0(self, interval: str, symbol: str, klines: List[list]):
         pass
 
     def _stream_update_with_start(self, symbols_body: SubscriberSymbolsBody):
         for interval, symbols in symbols_body.interval_symbols_map.items():
             self._save_klines(interval, symbols)
 
-    def _register_clean_redis_jobs(self, save_days: int):
-        self._scheduler.add_job(self._clean, id='clean_redis_4h', args=('4h', self._symbols, save_days),
-                                trigger='cron', hour='*/4')
-        self._scheduler.add_job(self._clean, id='clean_redis_2h', args=('2h', self._symbols, save_days),
-                                trigger='cron', hour='*/2')
-        self._scheduler.add_job(self._clean, id='clean_redis_1h', args=('1h', self._symbols, save_days),
-                                trigger='cron', hour='*')
-        self._scheduler.add_job(self._clean, id='clean_redis_30m', args=('30m', self._symbols, save_days),
-                                trigger='cron', minute='*/30')
-        self._scheduler.add_job(self._clean, id='clean_redis_15m', args=('15m', self._symbols, save_days),
-                                trigger='cron', minute='*/15')
-        self._scheduler.add_job(self._clean, id='clean_redis_5m', args=('5m', self._symbols, save_days),
-                                trigger='cron', minute='*/5')
-        self._scheduler.add_job(self._clean, id='clean_redis_3m', args=('3m', self._symbols, save_days),
-                                trigger='cron', minute='*/3')
-        self._scheduler.add_job(self._clean, id='clean_redis_1m', args=('1m', self._symbols, save_days),
-                                trigger='cron', minute='*')
+    def _register_clean_jobs(self, save_days: int):
+        pass
 
     def _save_latest_symbol_klines(self, symbol: str, interval: str, bar_count: int):
         klines = self.fetcher.get_klines(interval, symbol, bar_count)
-        last_kline_time = klines[-1][0]
-        last_interval_time[interval] = last_kline_time
 
         self._save_klines0(interval, symbol, klines)
         print(f'save {bar_count} klines success, symbol: {symbol}, interval: {interval}')
@@ -114,7 +96,7 @@ class Pusher(object):
     def start(self):
         self._start_stream_update()
         if self._clean_klines:
-            self._register_clean_redis_jobs(self._klines_save_days)
+            self._register_clean_jobs(self._klines_save_days)
         self._scheduler.start()
 
 
@@ -126,7 +108,7 @@ class RedisPusher(Pusher):
                                     db=redis_config['db_index'], password=redis_config['password'])
         self._redisc = StrictRedis(connection_pool=redis_pool)
 
-    def _push(self, klines: list):
+    def _push(self, klines: List[list]):
         super()._push(klines)
 
     def _clean(self, interval: str, symbols: List[str], save_days: int):
@@ -137,7 +119,7 @@ class RedisPusher(Pusher):
                 pipeline.zremrangebyscore(key, 0, now - (save_days * 1000 * 60 * 60 * 24))
             pipeline.execute()
 
-    def _set_kline(self, interval: str, symbol: str, kline, kline_time: int, remove: bool, insert: bool):
+    def _set_kline(self, interval: str, symbol: str, kline: list, kline_time: int, remove: bool, insert: bool):
         key = get_kline_key_name(self._namespace, interval, symbol)
         with self._redisc.pipeline(transaction=True) as pipeline:
             if remove:
@@ -150,7 +132,7 @@ class RedisPusher(Pusher):
         key = get_kline_key_name(self._namespace, interval, symbol)
         return self._redisc.zrangebyscore(key, start_time, start_time)
 
-    def _save_klines0(self, interval: str, symbol: str, klines: list):
+    def _save_klines0(self, interval: str, symbol: str, klines: List[list]):
         key = get_kline_key_name(self._namespace, interval, symbol)
         kline_score_mapping = {json.dumps(kline): kline[0] for kline in klines}
 
@@ -161,3 +143,23 @@ class RedisPusher(Pusher):
             pipeline.zadd(key, kline_score_mapping)
             pipeline.execute()
 
+    def _register_clean_jobs(self, save_days: int):
+        self._register_clean_redis_jobs(save_days)
+
+    def _register_clean_redis_jobs(self, save_days: int):
+        self._scheduler.add_job(self._clean, id='clean_redis_4h', args=('4h', self._symbols, save_days),
+                                trigger='cron', hour='*/4')
+        self._scheduler.add_job(self._clean, id='clean_redis_2h', args=('2h', self._symbols, save_days),
+                                trigger='cron', hour='*/2')
+        self._scheduler.add_job(self._clean, id='clean_redis_1h', args=('1h', self._symbols, save_days),
+                                trigger='cron', hour='*')
+        self._scheduler.add_job(self._clean, id='clean_redis_30m', args=('30m', self._symbols, save_days),
+                                trigger='cron', minute='*/30')
+        self._scheduler.add_job(self._clean, id='clean_redis_15m', args=('15m', self._symbols, save_days),
+                                trigger='cron', minute='*/15')
+        self._scheduler.add_job(self._clean, id='clean_redis_5m', args=('5m', self._symbols, save_days),
+                                trigger='cron', minute='*/5')
+        self._scheduler.add_job(self._clean, id='clean_redis_3m', args=('3m', self._symbols, save_days),
+                                trigger='cron', minute='*/3')
+        self._scheduler.add_job(self._clean, id='clean_redis_1m', args=('1m', self._symbols, save_days),
+                                trigger='cron', minute='*')
